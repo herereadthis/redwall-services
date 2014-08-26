0bd7406e01edaca5add6807403b1c8affd8932ed
Jimmy Ha <herereadthis@gmail.com>
2014-08-26 02:01:30 -0400

    #1 sort by hits

--- api/banner_image/index.php
+++ api/banner_image/index.php
@@ -27,16 +27,50 @@ $sql = "CREATE TABLE IF NOT EXISTS `banner_image` (
 $create_table = $db->prepare($sql);
 $create_table->execute();
 
-// prepare PDO
-// return the object with lowest hits as first result
-$get_objects=$db->prepare("SELECT * FROM `banner_image`;");
-// $get_objects=$db->prepare("SELECT * FROM `banner_image` ORDER BY `hits` ASC;");
-// execute
-$get_objects->execute();
-// fetch results as array
-$json_results=$get_objects->fetchAll(PDO::FETCH_ASSOC);
-// get objects from array and return as JSON
-$json=json_encode($json_results);
+
+$query_sort = $_GET["sort"];
+
+
+
+
+
+if ($query_sort === "hits") {
+    $lowest_hits = $db->prepare("SELECT * FROM `banner_image`
+    ORDER BY `hits` ASC;");
+    $lowest_hits->execute();
+    $lowest_hits_results=$lowest_hits->fetchAll(PDO::FETCH_ASSOC);
+    $first_result = $lowest_hits_results[0];
+    $unique_id = $first_result['unique_id'];
+    $hits = $first_result['hits'];
+    $hit_count = $hits + 1;
+
+    // $ljson=json_encode($lowest_hits_results);
+
+    // take the item of the top of the array and add one to the hit count
+    $add_hit = $db->prepare("UPDATE `banner_image`
+            SET `hits` = :hits WHERE `unique_id` = :unique_id;"); 
+    $add_hit->execute(array(
+        'hits' => $hit_count,
+        'unique_id' => $unique_id)
+    );
+
+    $hits_json = json_encode($lowest_hits_results);
+    echo $hits_json;
+}
+else {
+    // prepare PDO
+    // return the object with lowest hits as first result
+    $get_objects=$db->prepare("SELECT * FROM `banner_image`;");
+    // $get_objects=$db->prepare("SELECT * FROM `banner_image` ORDER BY `hits` ASC;");
+    // execute
+    $get_objects->execute();
+    // fetch results as array
+    $json_results=$get_objects->fetchAll(PDO::FETCH_ASSOC);
+    // get objects from array and return as JSON
+    $json=json_encode($json_results);
+    echo $json;
+}
+
 
 // $first_result = $json_results[0];
 // $unique_id = $first_result['unique_id'];
@@ -46,6 +80,7 @@ $json=json_encode($json_results);
 // echo $json;
 
 // grab row where hits are lowest
+/*
 $lowest_hits = $db->prepare("SELECT * FROM `banner_image`
     WHERE `hits` = (SELECT MIN(`hits`) FROM `banner_image`)");
 $lowest_hits->execute();
@@ -55,17 +90,27 @@ $first_result = $lowest_hits_results[0];
 $unique_id = $first_result['unique_id'];
 $hits = $first_result['hits'];
 $hit_count = $hits + 1;
+*/
+// $lowest_hits = $db->prepare("SELECT * FROM `banner_image`
+//     ORDER BY `hits` ASC;");
+// $lowest_hits->execute();
+// $lowest_hits_results=$lowest_hits->fetchAll(PDO::FETCH_ASSOC);
+// $first_result = $lowest_hits_results[0];
+// $unique_id = $first_result['unique_id'];
+// $hits = $first_result['hits'];
+// $hit_count = $hits + 1;
 
+// // $ljson=json_encode($lowest_hits_results);
 
-// take the item of the top of the array and add one to the hit count
-$add_hit = $db->prepare("UPDATE `banner_image`
-        SET `hits` = :hits WHERE `unique_id` = :unique_id;"); 
-$add_hit->execute(array(
-    'hits' => $hit_count,
-    'unique_id' => $unique_id)
-);
+// // take the item of the top of the array and add one to the hit count
+// $add_hit = $db->prepare("UPDATE `banner_image`
+//         SET `hits` = :hits WHERE `unique_id` = :unique_id;"); 
+// $add_hit->execute(array(
+//     'hits' => $hit_count,
+//     'unique_id' => $unique_id)
+// );
 
-echo $json;
+// echo $json;
 
 
 ?>
